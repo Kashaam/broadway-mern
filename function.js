f89e58d2da5e4d7b99c22da83f29c965c0fd2df5
Kashaam <kashamksh99@gmail.com>
2025-04-04 17:42:39 +0545

    Learning functions

---
+++ function.js
@@ -0,0 +1,39 @@
+function hello(a, b){
+    let n = a + b;
+    return n;
+}
+
+
+const maya = function(){
+    console.log("This is function variable")
+}
+
+
+const raman = (a,b, c) =>{
+    let m = a + b + c;
+    return m;
+}
+
+
+let result = hello(3, 6);
+maya()
+let lastResult = raman(3, 5, 6);
+
+console.log(result);
+console.log(lastResult);
+
+
+
+// hoisting
+
+console.log(sima);
+var sima = "Hello ) I am sima";
+
+
+
+
+
+foo()
+function foo(){
+    console.log("This is foo");
+}
