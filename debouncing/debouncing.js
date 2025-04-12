105749c954a03b9b053abc50901b96bc805cba57
Kashaam <kashamksh99@gmail.com>
2025-04-12 13:57:57 +0545

    debouncing definition

--- debouncing/debouncing.js
+++ debouncing/debouncing.js
@@ -25,3 +25,5 @@ const betterFunction = doSomeMagic(getData, 300);
 
 
 
+// debouncing in js is when api call is made through search bar we do not need to call on every keystroke  rather 
+// make some delay to call api , I mean when user stops then make a api call rather on every stroke.
